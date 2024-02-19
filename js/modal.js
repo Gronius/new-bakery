2f7917c6d3a87a90c7b5483fbb840f25628c7336
Vlad <vlivonika@gmail.com>
2024-02-19 10:24:21 +0100

    add mobile menu

--- js/modal.js
+++ js/modal.js
@@ -4,5 +4,17 @@ const modalBtnClose = document.querySelector('.modal-btn-close');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
+
+// modalBtnOpen.forEach(function (e) {
+//   e.addEventListener("click", toggleModal);
+// });
+
+// modalBtnClose.forEach(function (e) {
+//   e.addEventListener("click", toggleModal);
+// });
+
+
+
+
 modalBtnOpen.addEventListener('click', toggleModal);
 modalBtnClose.addEventListener('click', toggleModal);
